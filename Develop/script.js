783c16cac4e9ff46e17ada2b7d2865daf7e8c40a
Seval Cakir <sevalc@live.com>
2022-03-30 20:26:33 -0700

    prompts and charsets complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,55 @@
+
+
 function generatePassword(){
   //TODO: your code goes here
 
-  return "password"
+  // gather user preferences
+  var hasUppercase = confirm("Would you like any UpperCAse letters?");
+  var hasLowercase = confirm("Would you like any LowerCase letters?");
+  var hasSpecialChar = confirm("Would you like any special characters?");
+  var hasNums = confirm("Would you like any numbers?")
+  var pWLength = parseInt(prompt("Please enter password length btw 8-128.", "8"), 10)
+
+  // generate password characters
+  var upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lowerCaseSet = "abcdefghijklmnopqrstuvwxyz"
+  var numSet = "0123456789"
+  var specialSet = "!#$%_&- *"
+
+  var pWBuffer = []
+  if (hasUppercase) {
+    var randomChar = getRandomCharFromSet(upperCaseSet); 
+    pWBuffer.push(randomChar)
+  }
+  if (hasLowercase) {
+    var randomChar = getRandomCharFromSet(lowerCaseSet); 
+    pWBuffer.push(randomChar);
+  }
+  if (hasNums) {
+    var randomChar = getRandomCharFromSet(numSet);
+    pWBuffer.push(randomChar);
+  }
+  if (hasSpecialChar) {
+    var randomChar = getRandomCharFromSet(specialSet);
+    pWBuffer.push(randomChar);
+  }
+
+
+  for ( i=0; i < pWLength; i++) {
+
+    pWBuffer.push(getRandomCharFromSet(upperCaseSet));
+  }
+  // shuffle password chars
+
+
+
+  return pWBuffer.join("");
 }
 
+function getRandomCharFromSet(charSet){
+  var dice = Math.floor(Math.random() * charSet.length);
+  return charSet.charAt(dice);
+}
 
 // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
 var generateBtn = document.querySelector("#generate");
